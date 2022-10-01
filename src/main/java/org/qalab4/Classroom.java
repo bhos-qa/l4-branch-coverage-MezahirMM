48fba3903f932fa3c7a2adfbb8b1dae146a09c28
Mazahir Mammadov <mazahir.mammadov.std@bhos.edu.az>
2022-10-01 14:46:00 +0400

    committing

---
+++ src/main/java/org/qalab4/Classroom.java
@@ -0,0 +1,40 @@
+package org.qalab4;
+import java.io.IOException;
+
+public class Classroom {
+    public int multiply(int a, int b){
+        return a*b;
+    }
+    public int divide(int a, int b){
+        return a/b;
+    }
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int subtract(int a, int b){
+        return a -b;
+    }
+    public double square(double a){
+        return a*a;
+    }
+    public double squareRoot(double a) {
+        if (a >= 0)
+            return Math.sqrt(a);
+        return 0;
+    }
+    public boolean isOdd(int a) {
+        return a % 2 != 0;
+    }
+    public boolean isEven(int a) {
+        return a % 2 == 0;
+    }
+    public double logarithm(double a) {
+        return Math.log(a);
+    }
+    public double logarithmTen(double a){
+        return Math.log10(a);
+    }
+    public void startCalculator() throws IOException {
+        Runtime.getRuntime().exec("classroom.exe");
+    }
+}
---
+++ src/test/java/org/qalab4/ClassroomTest.java
@@ -0,0 +1,32 @@
+package org.qalab4;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClassroomTest {
+    Classroom classroom;
+    @BeforeEach
+    void setUp() {
+        classroom = new Classroom();
+    }
+    @Test
+    void testMultiply() {
+        assertEquals(39, classroom.multiply(39, 1), "Integer multiplication");
+    }
+    @Test
+    void testDivide() {
+        assertEquals(39, classroom.divide(39, 1), "Integer division");
+    }
+    @Test
+    void testAdd() {
+        assertEquals(40, classroom.add(39, 1), "Integer addition");
+    }
+    @Test
+    void testSubtract() {
+        assertEquals(38, classroom.subtract(39, 1), "Integer subtraction");
+    }
+    @Test
+    void testSquare() {
+        assertEquals(36, classroom.square(6), "Integer square");
+    }
+}
